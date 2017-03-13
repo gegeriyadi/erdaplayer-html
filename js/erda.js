2b969fcae3bf8db33621531192c6a905617eb956
Teguh Riyadi <gegeriyadi81@gmail.com>
2017-03-13 13:20:07 +0700

    first commit

---
+++ js/erda.js
@@ -0,0 +1,26 @@
+// Auto load
+function playStream(){
+	var aud = document.getElementById("audioStream");
+	$('#zen').addClass('play');
+	aud.play();
+	document.getElementById("status").innerHTML = "Status: ON";
+}
+
+// clik play/pause
+$(document).ready(function(){
+
+$("#zen").click(function(){
+var aud = document.getElementById("audioStream");
+	if (aud.paused) {
+		$('#zen').addClass('play');
+		aud.play();
+		document.getElementById("status").innerHTML = "Status: ON";
+	} else {
+		$('#zen .circle').removeClass( "rotate" );
+		$("#zen").removeClass( "play" );
+		aud.pause();
+		document.getElementById("status").innerHTML = "Status: OFF - Press Play button!";
+	}
+});
+
+});
